889fd7d0f4d7fcc5712d6bd6d85425d15b7718a5
Francis.Dev <francisgil.dev@gmail.com>
2023-12-12 14:54:59 +0800

    commit

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 //function to scrollView
-const scrollView =  () =>  {
-    //get the doms
-    const 
+const scrollView = () => {
+  
+    
+    
+    const scrollToSec =  (sectionIndex) =>  {
+    const sections = document.querySelectorAll('section');
+        if (sectionIndex >= 0 && sectionIndex < sections.length)
+        {sections[sectionIndex].scrollIntoView({behavior: 'smooth'})}
+    }
+    //addEvent Listener the navs
+    const scrollIcon = document.querySelector('#scroll');
+    const navMenu = document.querySelectorAll('ul li');
+    navMenu[1].addEventListener('click', () => scrollToSec(1))
+    navMenu[2].addEventListener('click', () => scrollToSec(2))
+    navMenu[3].addEventListener('click', () => scrollToSec(3))
+    //addEvent the scroll
+    scrollIcon.addEventListener('click', () => scrollToSec[1])
 
 }
-scrollView();
+window.addEventListener('DOMContentLoaded', scrollView);
+
+//function to toggleDark
+const toggleDark = () => {
+    //get the doms
+    const toggleIcon = document.querySelector('#toggleDark')
+
+    //addEvent
+    toggleIcon.addEventListener('click', () => {
+        const body = document.querySelector('body');
+        const submitIcon = document.querySelector('.bi-moon');
+        body.classList.toggle('bg-[#161A30]')
+        submitIcon.classList.replace(' bi-sun')
+    })
+}
+window.addEventListener('DOMContentLoaded',toggleDark);
+
+
+
